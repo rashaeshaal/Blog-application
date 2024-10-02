fdc0a21edff01996eca132a5f6c5bce2a8ac5fe8
rashaeshaal <rashaeshaal@gmail.com>
2024-10-02 17:14:12 +0530

    static file root url

--- backend/blogproject/blogproject/settings.py
+++ backend/blogproject/blogproject/settings.py
@@ -178,6 +178,8 @@ CORS_ALLOWED_ORIGINS = [
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
